a54b9019ed131e5ad8131f7de6d3da81dc76659a
Sarah Oladejo <oladejosarah234@gmail.com>
2022-11-23 14:55:00 -0500

    Update diffusion_parallel.go

--- diffusion_parallel.go
+++ diffusion_parallel.go
@@ -5,6 +5,7 @@ import (
 	"math/rand"
 	"runtime"
 	"time"
+	"fmt"
 )
 
 // calling BrownianMotion() to all particles in parallel
@@ -31,7 +32,7 @@ func (s *Surface) Diffuse(timeStep float64) {
 		time.Sleep(time.Nanosecond)
 		sourceY := rand.NewSource(time.Now().UnixNano())
 		generatorY := rand.New(sourceY)
-		go DiffuseOneProc(s.particles[startIndex:endIndex], generatorX, generatorY, finished, timeStep)
+		go DiffuseOneProc(s.particles[startIndex:endIndex], generatorX, generatorY, finished, s.width ,timeStep)
 	}
 
 	// we need to receive a message from all our channels that they're finished
@@ -40,22 +41,42 @@ func (s *Surface) Diffuse(timeStep float64) {
 	}
 
 }
-func DiffuseOneProc(particles []*Particle, generatorX, generatorY *(rand.Rand), finished chan bool, timeStep float64) {
+func DiffuseOneProc(particles []*Particle, generatorX, generatorY *(rand.Rand), finished chan bool, width , timeStep float64) {
 	//source := rand.NewSource(time.Now().UnixNano())
 	//generator := rand.New(source)
 	for _, p := range particles {
-		p.BrownianMotion(generatorX, generatorY, timeStep)
+		p.BrownianMotion(generatorX, generatorY, width, timeStep)
 	}
 	//function is done. Indicate this by placing true (or false) into channel
 	finished <- true
 }
 
 // Diffuse function update a Particle's displacement after 1 timeStep
-func (p *Particle) BrownianMotion(generatorX, generatorY *(rand.Rand), timeStep float64) {
+func (p *Particle) BrownianMotion(generatorX, generatorY *(rand.Rand), width, timeStep float64) {
 	std := math.Sqrt(2 * timeStep * p.species.diffusionRate)
 	dx := generatorX.NormFloat64() * std
 	dy := generatorY.NormFloat64() * std
 	p.x += dx
 	p.y += dy
+	if p.x > width || p.x < width || p.y > width || p.x < width{
+		p.SurfaceReaction(width)
+	}
 	//probably need to handle off boundary senario
+}
+
+func (p *Particle) SurfaceReaction(width float64) {
+	if p.x > width {
+		p.x = p.x - (p.x - width)
+	}else{
+		if p.x < 0{
+	  p.x = width - (p.x *(width/p.x))
+	}
+}
+	if p.y > width{
+		p.y = p.y - (p.y - width)
+	}else{
+		if p.y < 0{
+ 		p.y = width - (p.y *(width/p.y))
+	}
+ }
 }
