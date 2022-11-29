ef1877a83cc7e5cc3bbf38f9e0b48e373ec4cb03
rajeeganesan <rganesan@ucsd.edu>
2022-11-29 12:26:46 -0500

    Update functions.go

--- functions.go
+++ functions.go
@@ -21,15 +21,57 @@ func SimulateSurface(initialS *Surface, numGens int, time float64) []*Surface {
 
 // Surface method: Update()
 // Updates the Surface object given a time step.
-func (s *Surface) Update(time float64) *Surface {
+func (s *Surface) Update(time float64, rate float64) *Surface {
 	// create a copy of the current Surface object
 	newS := s.Copy()
 	// update the position of each particle
-	// for _, p := range newS.particles {
-	// 	p.UpdatePosition(time)
-	// }
+	for _, p := range newS.particles {
+	 	p = ZerothUpdatePosition(p,rate)
+	}
 	return newS
 }
+
+// in zeroth order reactions, the reaction progresses at a rate that is 
+// independent of all chemical concentrations. this means products
+// are formed spontaneously.
+
+// zeroth update position takes a particle and the underlying rate constant
+// updates position based simply on rate constant, with no relation to other particles
+// in the system
+func ZerothUpdatePosition(p Particle,rateConstant float64) OrderedPair {
+	var pos OrderedPair // initializes new position
+  std := math.Sqrt(2 * time * rateConstant)
+
+  if rateConstant > 1 {
+    // updates position based on rate constant
+    newParticle := p.CopyParticle
+    dx := generatorX.NormFloat64() * std
+    dy := generatorY.NormFloat64() * std
+    newParticle.x += dx
+    newParticle.y += dy  
+    return newParticle
+  }
+  
+  dx := generatorX.NormFloat64() * std
+  dy := generatorY.NormFloat64() * std
+  pos.x += dx
+  pos.y += dy
+
+  return pos
+}
+
+// particle method: Copy
+// creates a deep copy of the particle object at hand
+func (s *Particle) CopyParticle() *Particle {
+  var newP Particle
+  newP.position = s.position
+  newP.radius = s.radius
+  newP.red = s.red
+  newP.green = s.green
+  newP.blue = s.blue
+  return newP
+}
+
 
 // Surface method: Copy()
 // Creates a deep copy of the Surface object.
